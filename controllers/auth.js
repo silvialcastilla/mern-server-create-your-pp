58f30f4ae52f252840b049b8af92be9397d3ad30
Tropical23 <silope0302@gmail.com>
2020-09-28 13:18:49 +0200

    jwt finished

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,9 @@
 const jwt = require("../services/jwt");
 const moment = require("moment");
 const User = require("../models/user");
+const user = require("../models/user");
 
-function checkExpiredToken(token) {
+function willExpireToken(token) {
   const { exp } = jwt.decodedToken(token);
   const currentDate = moment().unix();
 
@@ -15,6 +16,28 @@ function checkExpiredToken(token) {
 function refreshAccessToken(req, res) {
   const { refreshToken } = req.body;
   const isTokenExpired = willExpireToken(refreshToken);
+
+  if (isTokenExpired) {
+    res.status(404).send({ message: "RefreshToken ha expirado" });
+  } else {
+    const { id } = jwt.decodedToken(refreshToken);
+    User.findOne({ _id: id }, (err, userStored) => {
+      if (err) {
+        res.status(500).send({ message: "Error del servidor" });
+      } else {
+        if (!user) {
+          res.status(404).send({ message: "Usuario no encontrado" });
+        } else {
+          res
+            .status(200)
+            .send({
+              accessToken: jwt.createAccessToken(userStored),
+              refreshToken: refreshToken,
+            });
+        }
+      }
+    });
+  }
 }
 
 module.exports = {
